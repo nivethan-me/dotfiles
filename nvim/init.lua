ee8dc49eaa524eb7a7fda70d52cbdcf1be3cfdb4
Nivethan-Ar <63112138+Nivethan-Ar@users.noreply.github.com>
2024-02-23 14:43:42 +0530

    feat(neovim): add nvim-treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -14,11 +19,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 
 }
 local opts = {}
@@ -28,6 +34,12 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript"},
+  highlight = { enable = true },
+  indent = { enable = true },  
+})
 
 require("catppuccin").setup()
 
