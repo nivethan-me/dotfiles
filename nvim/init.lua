b22f899276f5ed4c27475583232354db4a8efcc4
Nivethan-Ar <63112138+Nivethan-Ar@users.noreply.github.com>
2024-02-23 14:25:04 +0530

    feat(neovim): add telescope.nvim fuzzy finder

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -12,10 +14,20 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
 
 require("catppuccin").setup()
 
