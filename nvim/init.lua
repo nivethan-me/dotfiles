edbfa370f6ab1be99f2547cae9419c7b82a0bbee
Nivethan-Ar <63112138+Nivethan-Ar@users.noreply.github.com>
2024-02-23 19:11:54 +0530

    feat(neovim): add neo-tree.nvim for file tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,7 +24,16 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
+    }
+  }
 
 }
 local opts = {}
